a13c7ad8161909ffb97f914f4213059334a4ebc5
David Silva <srdavidsilva@gmail.com>
2024-10-29 17:37:51 -0300

    testing: version

--- coeur/__init__.py
+++ coeur/__init__.py
@@ -1,6 +1,7 @@
 import toml
 import os
 
+# it will be replaced with the tag version on pipeline, this settings is only for local work
 pyproject_data = toml.load(os.path.join(os.path.dirname(__file__), "../pyproject.toml"))
 version = pyproject_data["tool"]["poetry"]["version"]
 
