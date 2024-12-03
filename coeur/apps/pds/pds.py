2faf7c0e4e0f561d4e38f3e1bc0fee11541f788d
David Silva <srdavidsilva@gmail.com>
2024-12-03 17:13:34 -0300

    feat: adding support to bluesky

--- coeur/apps/pds/pds.py
+++ coeur/apps/pds/pds.py
@@ -8,5 +8,7 @@ from typing import List
 
 
 @app.command()
-def publish(channels: List[Channels], total: int = 1, post_footer: str = None) -> None:
-    Engine(channels, total, post_footer).run()
+def publish(
+    channels: List[Channels], total: int = 1, post_header: str = None, post_footer: str = None
+) -> None:
+    Engine(channels, total, post_header, post_footer).run()
